c2f023467441674937f623e84ac30e0549f2f8ac
Jack Schwarz <jackschwarz58@gmail.com>
2020-06-19 13:20:38 -0500

    Replaced 'sessionCard' component with parent 'cardContainer' compoenent.
    As a result of this, the sessionCard import was also removed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "./components/navbar";
 import "./App.css";
-import SessionCard from "./components/sessionCard";
+import CardContainer from "./components/cardContainer";
 
 class App extends Component {
   state = {
@@ -12,14 +12,36 @@ class App extends Component {
         userName: "Jack58",
       },
     ],
+    sessions: [
+      {
+        id: 1,
+        title: "Example Session Title 1",
+        date: "Monday June 29, 2020 at 7:50pm",
+        desc: "This is an example session. This should be the first one",
+        attendeeCount: 14,
+      },
+      {
+        id: 2,
+        title: "Example Session Title 2",
+        date: "Monday June 25, 2020 at 12:50am",
+        desc: "This is an example session. This should be the second one",
+        attendeeCount: 149,
+      },
+      {
+        id: 3,
+        title: "Example Session Title 3",
+        date: "Wednesday June 27, 2020 at 12:50am",
+        desc: "This is an example session. This should be the third one",
+        attendeeCount: 85,
+      },
+    ],
   };
 
   render() {
     return (
       <React.Fragment>
         <NavBar user={this.state.users[0]} />
-        <SessionCard />
-        <SessionCard />
+        <CardContainer sessionCards={this.state.sessions} />
       </React.Fragment>
     );
   }
