8e349bb138b69f90cc1aadd1c1c91001de3b9704
Jack Schwarz <jackschwarz58@gmail.com>
2020-06-19 13:21:47 -0500

    Replaced dummy text with text passed in parent container.
    This is just replacing the dummy text I had put in for testing with actual dynamically created code from an array of sessions created in App.js. Eventually this will be a backend call to find user specific sessions.

--- src/components/sessionCard.jsx
+++ src/components/sessionCard.jsx
@@ -5,22 +5,17 @@ import { faEdit, faTrash } from "@fortawesome/free-solid-svg-icons";
 class SessionCard extends Component {
   state = {};
   render() {
+    const { title, date, desc, attendeeCount } = this.props;
     return (
       <div className="container bg-light my-4 p-3 rounded shadow">
         <div className="row">
           <div className="col col-10">
-            <h2>Online Learning Book Discussion</h2>
-            <h5 className="text-secondary">Monday June 22, 2020 at 11:30am</h5>
-            <p className="mt-4">
-              This is where a description goes and you know you'll like it when
-              you see it! Wow this is getting pretty long if you ask me. Maybe I
-              should drop in some Latin for good mesaure. Lorem ipsum dolor sit
-              amet, consectetur adipiscin- you know what? Nevermind. Lets just
-              pad this out a bit to get another line wrapped to make sure. Such
-              a pretty house and such a pretty garden. No alarms and No suprises
-              please.
-            </p>
-            <span className="mt-4 text-muted font-italic">Attendees: 12</span>
+            <h2>{title}</h2>
+            <h5 className="text-secondary">{date}</h5>
+            <p className="mt-4">{desc}</p>
+            <span className="mt-4 text-muted font-italic">
+              Attendees: {attendeeCount}
+            </span>
           </div>
           <div className="col col-2 text-secondary">
             <div>
