cd812029ea524b2c2a08333815038ae133890d55
Jack Schwarz <jackschwarz58@gmail.com>
2020-06-23 15:05:48 -0500

    Defined img size for window resizing and added body text label

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 
 class SideBar extends Component {
-  state = {};
   render() {
     return (
       <nav id="sidebar">
@@ -11,15 +10,20 @@ class SideBar extends Component {
               <img
                 className="sidebar-img"
                 src={`../assets/brand-logo.jpg`}
+                height="133px"
+                width="133px"
                 alt="???"
                 loading="lazy"
               />
               <div>
-                <h5 className="mt-1">Paycom Summer Engagement Program</h5>
+                <h6 className="mt-1">Paycom Summer Engagement Program</h6>
               </div>
             </div>
           </div>
         </div>
+        <h6 className="p-3 text-center" id="sidebar-session-header">
+          Upcoming Sessions
+        </h6>
       </nav>
     );
   }
