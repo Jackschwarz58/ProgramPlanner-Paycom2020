4fafea5a9a05070d69b43752477ecdd24aa40d05
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-01 23:55:40 -0500

    Moved datePicker css import here

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import "react-datepicker/dist/react-datepicker.css";
 import "./index.css";
 
 ReactDOM.render(
