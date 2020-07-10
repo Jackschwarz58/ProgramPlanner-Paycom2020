337d9c0adc90b2645dc1b6ebaf3c6b97c4613fea
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-10 00:18:25 -0500

    Added Redux store to keep track of user credentials

---
+++ src/store/actions/userActions.js
@@ -0,0 +1,5 @@
+function update_login(givenType, givenPayload) {
+  return { type: givenType, payload: givenPayload };
+}
+
+export default update_login;
---
+++ src/store/reducers/userReducer.js
@@ -0,0 +1,23 @@
+function userReducer(state = {}, { type, payload }) {
+  console.log(type, "    ", payload);
+  switch (type) {
+    case "LOGIN":
+      return {
+        loggedIn: true,
+        uid: payload.uid,
+        userName: payload.userName,
+        email: payload.email,
+      };
+    case "LOGOUT":
+      return {
+        loggedIn: false,
+        uid: "",
+        userName: "",
+        email: "",
+      };
+    default:
+      return state;
+  }
+}
+
+export default userReducer;
