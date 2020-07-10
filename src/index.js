e33de448124ba3b97e04222bfc6695c8391c96f4
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-10 00:18:49 -0500

    Restructured to implement routing

--- src/index.js
+++ src/index.js
@@ -2,13 +2,27 @@ import React from "react";
 import ReactDOM from "react-dom";
 import LoginApp from "./components/LoginApp";
 import DashboardApp from "./components/DashboardApp";
+import PrivateRoute from "./components/Routes/PrivateRoute";
 import "react-datepicker/dist/react-datepicker.css";
 import "./css/index.css";
+import store from "./store/index";
+import { Provider } from "react-redux";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <LoginApp />
-    {/* <DashboardApp /> */}
-  </React.StrictMode>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/index" component={LoginApp} />
+
+        <PrivateRoute
+          path="/dashboard"
+          component={DashboardApp}
+          isAuth={true}
+        />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+
   document.getElementById("root")
 );
