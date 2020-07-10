d2ed07c07cdda44e953a9e8764c09bb36ac89244
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-10 00:24:44 -0500

    Added Redux calls, created userlogin functions, added props to child components
    This was another file I was tracking in a seperate private repo when testing server calls. Therefore this one seems slightly behind on commits.

--- src/components/LoginApp.js
+++ src/components/LoginApp.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import Login from "./Login/login";
 import Register from "./Register/register";
+import store from "../store/index";
+import update_login from "../store/actions/userActions";
+import { connect } from "react-redux";
 
 class LoginApp extends Component {
   state = { isLogin: true };
@@ -11,21 +14,43 @@ class LoginApp extends Component {
     }));
   };
 
+  updateLogin = (type, payload) => {
+    store.dispatch(
+      update_login("LOGIN", {
+        loggedIn: payload.loggedIn,
+        uid: payload.uid,
+        userName: payload.userName,
+        email: payload.email,
+      })
+    );
+  };
+
   render() {
     if (!this.state.isLogin) {
       return (
         <React.Fragment>
-          <Register handleCompChange={this.handleCompChange} />
+          <Register
+            handleCompChange={this.handleCompChange}
+            updateLogin={this.updateLogin}
+          />
         </React.Fragment>
       );
     } else {
       return (
         <React.Fragment>
-          <Login handleCompChange={this.handleCompChange} />
+          <Login
+            {...this.props}
+            handleCompChange={this.handleCompChange}
+            updateLogin={this.updateLogin}
+          />
         </React.Fragment>
       );
     }
   }
 }
 
-export default LoginApp;
+const mapStateToProps = (state) => {
+  return state;
+};
+
+export default connect(mapStateToProps)(LoginApp);
