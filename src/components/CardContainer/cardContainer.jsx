41c2522fc8460ab509f0a0cc0cf20e316505c43f
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-13 00:36:32 -0500

    Added comments

--- src/components/CardContainer/cardContainer.jsx
+++ src/components/CardContainer/cardContainer.jsx
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
-import SessionCard from "../SessionCard/sessionCard";
-import "./cardContainer.css";
+import SessionCard from "../SessionCard/sessionCard"; //Session card objects
+import "./cardContainer.css"; //Styling
 
+//Holds all of the session cards and sets them up within the dashboard
 class CardContainer extends Component {
   render() {
-    const { sessionCards, onDelete, onEdit } = this.props;
+    const { sessionCards, onDelete, onEdit } = this.props; //To avoid repeated code
     return (
       <div id="card-container">
         {sessionCards.map((sessionCard) => (
+          //All of these are the attributes used in a session card and all of them are different depending on the session information
           <SessionCard
             key={sessionCard.id}
             id={sessionCard.id}
