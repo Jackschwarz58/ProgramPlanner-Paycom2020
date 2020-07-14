4ddaf8c5d839857886ee0ea70e9a1b4923bc3a02
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-13 23:06:53 -0500

    Changed syntax to match API calls and added in onEdit prop for sessionCard editing

--- src/components/CardContainer/cardContainer.jsx
+++ src/components/CardContainer/cardContainer.jsx
@@ -5,20 +5,21 @@ import "./cardContainer.css"; //Styling
 //Holds all of the session cards and sets them up within the dashboard
 class CardContainer extends Component {
   render() {
-    const { sessionCards, onDelete, onEdit } = this.props; //To avoid repeated code
+    const { sessions, onDelete, onEdit, handleFieldChange } = this.props; //To avoid repeated code
     return (
       <div id="card-container">
-        {sessionCards.map((sessionCard) => (
+        {sessions.map((session) => (
           //All of these are the attributes used in a session card and all of them are different depending on the session information
           <SessionCard
-            key={sessionCard.id}
-            id={sessionCard.id}
-            title={sessionCard.title}
-            dateTime={sessionCard.dateTime}
-            desc={sessionCard.desc}
-            attendeeCount={sessionCard.attendeeCount}
+            key={session.sessionId}
+            id={session.sessionId}
+            title={session.sessionName}
+            dateTime={session.sessionTime}
+            desc={session.sessionDesc}
+            attendeeCount={session.sessionAttendees}
             onDelete={onDelete}
             onEdit={onEdit}
+            handleFieldChange={handleFieldChange}
           ></SessionCard>
         ))}
       </div>
