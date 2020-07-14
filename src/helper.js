c85c80ccaa11b1a56961c44a75077e3410ef7ed2
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-13 23:01:05 -0500

    Moved almost all api calls into this file. Added calls for session events such as add, edit, delete.
    All of the api calls made sense to be put in one consolidated file and so I have begun moving all the php calls into helper and dealing with their errors elsewhere. Eventually this will be the apps  only interface to the api, but I'm still working on that :).

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,6 @@
 import store from "./store/index";
 import update_login from "./store/actions/userActions";
+import axios from "axios"; //Nicer API calls
 
 export function updateLogin(type, payload) {
   //Updates the store with the given user information and effectively "logs" the user in all throughout the application
@@ -17,4 +18,62 @@ export function updateLogin(type, payload) {
     case "LOGOUT":
       break;
   }
+}
+
+export function checkLogin() {
+  return axios({
+    method: "post",
+    url: "http://192.168.64.2/paycomProject/api/check.php",
+  }).then(({ data, status }) => {
+    //API defined success status number
+    //Logs the user back in and sets store vars
+    if (status === 201) {
+      updateLogin("LOGIN", {
+        loggedIn: true,
+        uid: data.login_usr_id,
+        userName: data.login_usr_name,
+        email: data.login_usr_email,
+      });
+    }
+    return status;
+  });
+}
+
+export function getSessions() {
+  return axios({
+    method: "post",
+    url: "http://192.168.64.2/paycomProject/api/sessions.php",
+    data: {
+      functionname: "getSessions",
+    },
+  }).then((results) => {
+    return results;
+  });
+}
+
+export function editSession(attributes) {
+  return axios({
+    method: "post",
+    url: "http://192.168.64.2/paycomProject/api/sessions.php",
+    data: {
+      functionname: "editSession",
+      session: attributes,
+    },
+  }).then((results) => {
+    return true;
+  });
+}
+
+export function deleteSession(id) {
+  return axios({
+    method: "post",
+    url: "http://192.168.64.2/paycomProject/api/sessions.php",
+    data: {
+      functionname: "deleteSession",
+      sessionId: id,
+    },
+  }).then((results) => {
+    console.log(results);
+    return true;
+  });
 }
