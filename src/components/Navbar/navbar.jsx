da2e55f5370cebfc4a202c61b39e9e5d12a71268
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-15 13:35:33 -0500

    Added ability to join existing sessions and restyled the navbar options
    With the user specific data implemented, I added the ability to view a full list of sessions and select which one you want to join

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,13 +1,48 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; //For nice glyphs on the buttons
-import { faCalendarPlus } from "@fortawesome/free-solid-svg-icons"; //Add session button
+import { faCalendar, faCalendarPlus } from "@fortawesome/free-solid-svg-icons"; //Add session button
 import "./navbar.css"; //Styling
+import { getAllSessions } from "../../helper";
+import { format } from "date-fns"; //Nicer Date formatting. Didn't want to use Moment, wayy to big
 
 //The top bar on the component page. This is where users can log out or add a session. This bar also contains the user name and a template photo for the user.
 class NavBar extends Component {
+  state = { sessionList: [] };
+  constructor() {
+    super();
+    this.buildExistingSessions = this.buildExistingSessions.bind(this);
+  }
+
+  componentDidMount() {
+    getAllSessions().then(({ data }) => {
+      console.log(data);
+      this.setState({ sessionList: data });
+    });
+  }
+
+  buildExistingSessions() {
+    return (
+      <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+        {this.state.sessionList.map((session) => (
+          <a
+            className="dropdown-item pt-3 session-selector"
+            key={session.sessionId}
+            onClick={() => this.props.onJoinSession(session.sessionId)}
+          >
+            <h6>{session.sessionName}</h6>
+            <p className="small-font">
+              {format(session.sessionTime, "eeee MMMM do, yyyy 'at' h:mmaaa")}
+            </p>
+          </a>
+        ))}
+      </div>
+    );
+  }
+
   render() {
     const { user, onAdd } = this.props; //To avoid repeated code
+
     return (
       <div>
         <nav
@@ -31,21 +66,35 @@ class NavBar extends Component {
             className="collapse navbar-collapse pl-2"
             id="navbarSupportedContent"
           >
-            <ul className="navbar-nav ">
+            <ul className="navbar-nav">
               <li className="nav-item">
-                <button
-                  type="button"
-                  className="btn btn-sm top-action-btn"
-                  onClick={() => onAdd()} //Calls the parent-located function to add a new session
+                <a
+                  className="nav-link active nav-action rounded"
+                  onClick={() => onAdd()}
                 >
-                  <a className="text-dark">
-                    <FontAwesomeIcon icon={faCalendarPlus} /> Add Session
-                  </a>
-                </button>
+                  <FontAwesomeIcon icon={faCalendarPlus} id="nav-icon" />
+                  Add New Session
+                </a>
+              </li>
+
+              <li className="nav-item active dropdown">
+                <a
+                  className="nav-link dropdown-toggle nav-action rounded"
+                  href="#"
+                  id="navbarDropdownMenuLink"
+                  data-toggle="dropdown"
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                >
+                  <FontAwesomeIcon icon={faCalendar} id="nav-icon" />
+                  Join Existing Session
+                </a>
+                {this.buildExistingSessions()}
               </li>
             </ul>
-            <div className="navbar-nav ml-auto top-action-btn rounded pl-2">
-              <a className="navbar-brand" href="#">
+
+            <div className="ml-auto rounded pl-2">
+              <span className="navbar-brand">
                 <img
                   className="rounded-circle"
                   src={`/paycomProject/assets/img/default-profile.jpg`} //I wish this was customizable but I did not think it was a necessity to spend a ton of time on
@@ -57,7 +106,7 @@ class NavBar extends Component {
                 <span className="text-dark" id="user-name-label">
                   {user.userName} {/*From the defined user in the Redux store*/}
                 </span>
-              </a>
+              </span>
             </div>
           </div>
         </nav>
