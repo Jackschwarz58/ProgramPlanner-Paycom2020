43c2f1d03d3561b88acac4810df4b0144941d8d8
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-14 20:23:26 -0500

    Changed Private Route to regular route for dashboard.
    The use of a 'PrivateRoute' caused some issues when trying to unify the login check function. So i got rid of it and added a redirect option to the dashboard component

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import store from "./store/index";
 import { Provider } from "react-redux";
 //Routing:
 import { BrowserRouter, HashRouter, Route, Switch } from "react-router-dom";
-import PrivateRoute from "./components/Routes/PrivateRoute";
 //Components:
 import Login from "./components/Login/login";
 import Register from "./components/Register/register";
@@ -23,11 +22,7 @@ ReactDOM.render(
         {/*The register page. Users create accounts here and have the option to be sent back to login*/}
         <Route path="/register" render={(props) => <Register />} />
         {/*Main dashboard view. This is where users interact with sessions and are redirected to login if they chooser to logout of their session. This route is private as the dashboard should not be entered if no user is present*/}
-        <PrivateRoute
-          path="/dashboard"
-          component={DashboardApp}
-          isAuth={true} //IMPLEMENT
-        />
+        <Route path="/dashboard" render={(props) => <DashboardApp />} />
       </Switch>
     </BrowserRouter>
   </Provider>,
