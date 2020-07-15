83e5923cc093cadb1b1d83509024201d79caba30
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-14 20:29:25 -0500

    Added Login check and redirect to see if the user is logged in and if they arent.

--- src/components/DashboardApp.js
+++ src/components/DashboardApp.js
@@ -4,19 +4,34 @@ import CardContainer from "./CardContainer/cardContainer";
 import SideBar from "./Sidebar/sidebar";
 import store from "../store/index";
 import { connect } from "react-redux";
-import { getSessions, addSession, editSession, deleteSession } from "../helper";
+import { Redirect } from "react-router-dom"; //To connect to other pages in the app
+import {
+  getSessions,
+  addSession,
+  editSession,
+  deleteSession,
+  checkLogin,
+} from "../helper";
 
 class DashboardApp extends Component {
-  state = { error: null, sessions: [] };
+  state = { error: null, sessions: [], toLogin: false };
 
   componentDidMount() {
-    this.updateSessionData();
+    checkLogin().then((status) => {
+      if (status === 201) {
+        this.updateSessionData();
+      } else {
+        this.setState({ toLogin: true }); //Set redirect flag
+      }
+    });
   }
 
   updateSessionData = () => {
     getSessions()
       .then(({ data }) => {
-        data.sort((a, b) => (a.dateTime > b.dateTime ? -1 : 1));
+        if (!data.length === 0) {
+          data.sort((a, b) => (a.dateTime > b.dateTime ? -1 : 1));
+        }
         this.setState({ sessions: data });
       })
       .catch((e) => {
@@ -80,20 +95,24 @@ class DashboardApp extends Component {
   };
 
   render() {
-    return (
-      <React.Fragment>
-        <SideBar sessions={this.state.sessions} />
-        <div id="wrapper">
-          <NavBar user={store.getState()} onAdd={this.handleNewSession} />
-          <CardContainer
-            sessions={this.state.sessions}
-            onDelete={this.handleDelete}
-            onEdit={this.handleEdit}
-            handleFieldChange={this.handleFieldChange}
-          />
-        </div>
-      </React.Fragment>
-    );
+    if (this.state.toLogin === true) {
+      return <Redirect to="/index" />;
+    } else {
+      return (
+        <React.Fragment>
+          <SideBar sessions={this.state.sessions} />
+          <div id="wrapper">
+            <NavBar user={store.getState()} onAdd={this.handleNewSession} />
+            <CardContainer
+              sessions={this.state.sessions}
+              onDelete={this.handleDelete}
+              onEdit={this.handleEdit}
+              handleFieldChange={this.handleFieldChange}
+            />
+          </div>
+        </React.Fragment>
+      );
+    }
   }
 }
 
