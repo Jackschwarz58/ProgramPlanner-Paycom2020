ecfb96915c95282e6c9193a004b4ea6795d89835
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-14 20:28:07 -0500

    Added case for when no session data is present.
    This situation wasnt previously handled, so I added a case where a little text blurb is displayed if no sessions are found in the DB.

--- src/components/CardContainer/cardContainer.jsx
+++ src/components/CardContainer/cardContainer.jsx
@@ -6,27 +6,36 @@ import "./cardContainer.css"; //Styling
 class CardContainer extends Component {
   render() {
     const { sessions, onDelete, onEdit, handleFieldChange } = this.props; //To avoid repeated code
-    return (
-      <div id="card-container">
-        {sessions.map((session) => (
-          //All of these are the attributes used in a session card and all of them are different depending on the session information
-          <SessionCard
-            key={session.sessionId}
-            id={session.sessionId}
-            title={session.sessionName}
-            dateTime={session.sessionTime}
-            desc={session.sessionDesc}
-            attendeeCount={session.sessionAttendees}
-            onDelete={onDelete}
-            onEdit={onEdit}
-            handleFieldChange={handleFieldChange}
-            isEditing={
-              session.hasOwnProperty("isEditing") ? session.isEditing : false
-            }
-          />
-        ))}
-      </div>
-    );
+
+    if (sessions.length === 0) {
+      return (
+        <div className="text-secondary text-center" id="card-container">
+          Looks Like You Have a Clear Schedule!
+        </div>
+      );
+    } else {
+      return (
+        <div id="card-container">
+          {sessions.map((session) => (
+            //All of these are the attributes used in a session card and all of them are different depending on the session information
+            <SessionCard
+              key={session.sessionId}
+              id={session.sessionId}
+              title={session.sessionName}
+              dateTime={session.sessionTime}
+              desc={session.sessionDesc}
+              attendeeCount={session.sessionAttendees}
+              onDelete={onDelete}
+              onEdit={onEdit}
+              handleFieldChange={handleFieldChange}
+              isEditing={
+                session.hasOwnProperty("isEditing") ? session.isEditing : false
+              }
+            />
+          ))}
+        </div>
+      );
+    }
   }
 }
 
