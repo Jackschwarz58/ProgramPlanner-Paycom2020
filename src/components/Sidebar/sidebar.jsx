dbc05be3f39dda9caa1c80c032649743209bb258
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-14 20:25:26 -0500

    Split off list writing to seperate func, added logout button, and added handler for no session state
    I had an issue since I was not handling when there are no session present, so I went ahead and added a case handler for that where data is relied on. I also added a logout button here for the upcoming user logout funcationality. Styling comes with this

--- src/components/Sidebar/sidebar.jsx
+++ src/components/Sidebar/sidebar.jsx
@@ -1,8 +1,27 @@
 import React, { Component } from "react";
 import "./sidebar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; //Needed for icons
+import { faSignOutAlt } from "@fortawesome/free-solid-svg-icons"; //For nice buttons
 
 //Sidebar on the Dashboard page that displays the brand logo (defined below), the event title, and a list of user sessions ordered by time from the current time.
 class SideBar extends Component {
+  buildSessionsList = () => {
+    if (!this.props.sessions.length == 0) {
+      return this.props.sessions.map((session) => (
+        <li className="session-list-item rounded" key={session.sessionId}>
+          <a
+            className="w-100 text-left full-width"
+            href={"#" + session.sessionId}
+          >
+            {session.sessionName}
+          </a>
+        </li>
+      ));
+    } else {
+      return "";
+    }
+  };
+
   render() {
     return (
       //Makes heavy use of bootstrap classes
@@ -30,12 +49,14 @@ class SideBar extends Component {
           </h6>
           <ul id="upcoming-session-list">
             {/*This just Maps the defined sessions into a list that is set with a styling class and key*/}
-            {this.props.sessions.map((session) => (
-              <li className="session-list-item rounded" key={session.sessionId}>
-                <a href={"#" + session.sessionId}>{session.sessionName}</a>
-              </li>
-            ))}
+            {this.buildSessionsList()}
           </ul>
+          <div id="btm-container">
+            <button className="w-100 text-left rounded" id="signout-btn">
+              <FontAwesomeIcon icon={faSignOutAlt} />
+              &nbsp;Sign Out
+            </button>
+          </div>
         </div>
       </nav>
     );
