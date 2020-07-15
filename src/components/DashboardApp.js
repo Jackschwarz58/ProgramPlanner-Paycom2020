d35899d7d1e7e69bee48124cbdb479b589eaf01d
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-15 13:35:05 -0500

    Added error handler for getting session list, added Date Change handler, and logout handler

--- src/components/DashboardApp.js
+++ src/components/DashboardApp.js
@@ -6,31 +6,40 @@ import store from "../store/index";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom"; //To connect to other pages in the app
 import {
-  getSessions,
+  getUserSessions,
+  getAllSessions,
   addSession,
   editSession,
   deleteSession,
   checkLogin,
+  updateLogin,
 } from "../helper";
 
 class DashboardApp extends Component {
-  state = { error: null, sessions: [], toLogin: false };
+  state = { error: null, sessions: [], toLogin: false, showModal: false };
 
   componentDidMount() {
-    checkLogin().then((status) => {
-      if (status === 201) {
-        this.updateSessionData();
-      } else {
-        this.setState({ toLogin: true }); //Set redirect flag
-      }
-    });
+    getAllSessions();
+    checkLogin()
+      .then((status) => {
+        if (status === 201) {
+          this.updateSessionData();
+        } else {
+          this.setState({ toLogin: true }); //Set redirect flag
+        }
+      })
+      .catch((e) => {
+        this.setState({ error: e }, () => {
+          window.alert(e + " \n" + e.response.statusText);
+        });
+      });
   }
 
   updateSessionData = () => {
-    getSessions()
+    getUserSessions()
       .then(({ data }) => {
-        if (!data.length === 0) {
-          data.sort((a, b) => (a.dateTime > b.dateTime ? -1 : 1));
+        if (!data.length == 0) {
+          data.sort((a, b) => (a.sessionTime > b.sessionTime ? 1 : -1));
         }
         this.setState({ sessions: data });
       })
@@ -94,20 +103,65 @@ class DashboardApp extends Component {
     this.setState({ sessions });
   };
 
+  handleLogout = () => {
+    updateLogin("LOGOUT", {})
+      .then(({ status, statusText }) => {
+        if (status === 203) {
+          this.setState({ toLogin: true }); //Set redirect flag
+        } else {
+          window.alert(
+            "Something Went Wrong :(\n" + status + " \n" + statusText
+          );
+        }
+      })
+      .catch((e) => {
+        this.setState({ error: e }, () => {
+          window.alert(e + " \n" + e.response.statusText);
+        });
+      });
+  };
+
+  handleDateChange = (dateVal, id) => {
+    console.log(dateVal.getTime() / 1000);
+    const index = this.state.sessions.findIndex((s) => {
+      return s.sessionId === id;
+    });
+
+    const sessions = [...this.state.sessions];
+    const item = {
+      ...sessions[index],
+      sessionTime: dateVal.getTime(),
+    };
+    sessions[index] = item;
+    this.setState({ sessions });
+  };
+
+  handleJoinSession = (id) => {
+    console.log("Session Selected: " + id);
+  };
+
   render() {
     if (this.state.toLogin === true) {
       return <Redirect to="/index" />;
     } else {
       return (
         <React.Fragment>
-          <SideBar sessions={this.state.sessions} />
+          <SideBar
+            sessions={this.state.sessions}
+            onLogout={this.handleLogout}
+          />
           <div id="wrapper">
-            <NavBar user={store.getState()} onAdd={this.handleNewSession} />
+            <NavBar
+              user={store.getState()}
+              onAdd={this.handleNewSession}
+              onJoinSession={this.handleJoinSession}
+            />
             <CardContainer
               sessions={this.state.sessions}
               onDelete={this.handleDelete}
               onEdit={this.handleEdit}
-              handleFieldChange={this.handleFieldChange}
+              onDateChange={this.handleDateChange}
+              onFieldChange={this.handleFieldChange}
             />
           </div>
         </React.Fragment>
