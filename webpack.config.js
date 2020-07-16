d822895eca4693349b373f141516e1d8a597896b
Jack Schwarz <jackschwarz58@gmail.com>
2020-07-16 02:19:48 -0500

    Added Config files for webpack

---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+"use strict";
+
+const webpack = require("webpack");
+const path = require("path");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+let config = {
+  entry: {
+    main: ["./_devapp/index.js", "./_devapp/css/index.css"],
+  },
+  output: {
+    path: path.resolve(__dirname, "build", "bundle"),
+    filename: "[name].bundle.js",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+          plugins: ["@babel/transform-runtime"],
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [
+            {
+              loader: "css-loader",
+            },
+            "postcss-loader",
+            "sass-loader",
+          ],
+        }),
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: "file-loader",
+      },
+    ],
+  },
+  externals: {
+    myApp: "myApp",
+  },
+};
+
+if (process.env.NODE_ENV === "production") {
+  config.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: false,
+      compress: {
+        sequences: true,
+        conditionals: true,
+        booleans: true,
+        if_return: true,
+        join_vars: true,
+        drop_console: true,
+      },
+      output: {
+        comments: false,
+      },
+      minimize: true,
+    })
+  );
+}
+
+module.exports = config;
